c435f95a77b0948ee8a51850c768e53b62661a73
filedw64 <1356100126@qq.com>
2026-07-16 23:36:08 +0800

    Update Pam's Harvest Integration

--- src/main/java/moze_intel/projecte/integration/PHCInit.java
+++ src/main/java/moze_intel/projecte/integration/PHCInit.java
@@ -1,21 +1,62 @@
 package moze_intel.projecte.integration;
 
 import moze_intel.projecte.emc.mappers.IntegrationMapper;
-import moze_intel.projecte.emc.mappers.OreDictionaryMapper;
 import moze_intel.projecte.utils.ItemHelper;
+import moze_intel.projecte.utils.PELogger;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class PHCInit {
     public static void init() {
-        OreDictionaryMapper.addBlacklistException("dustSalt");
+
+        IntegrationMapper.addMapping("harvestcraft:beeswaxItem", 0, 256);
+        IntegrationMapper.addMapping("harvestcraft:honeyItem", 0, 32);
+        IntegrationMapper.addMapping("harvestcraft:berrygarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:herbgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:groundgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:desertgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:grassgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:gourdgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:stalkgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:watergarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:textilegarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:mushroomgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:tropicalgarden", 0, 384);
+        IntegrationMapper.addMapping("harvestcraft:leafygarden", 0, 384);
+
         for (ItemStack stack : ItemHelper.getODItems("listAllseed")) {
-            if (stack == null || stack.getItem() == null)
-                continue;
+            if (stack == null || stack.getItem() == null) continue;
             String id = Item.itemRegistry.getNameForObject(stack.getItem());
             if (id.startsWith("harvestcraft:") && id.endsWith("seedItem")) {
                 IntegrationMapper.addMapping(stack, 16);
-                IntegrationMapper.addMapping(id.replace("seed",""),0, 64);
+            }
+        }
+
+        for (ItemStack stack : ItemHelper.getODItems("listAllfishraw")) {
+            if (stack == null || stack.getItem() == null) continue;
+            String id = Item.itemRegistry.getNameForObject(stack.getItem());
+            if (id.startsWith("harvestcraft:") && id.endsWith("Item")) {
+                IntegrationMapper.addMapping(stack, 64);
+            }
+        }
+
+        for (ItemStack stack : ItemHelper.getODItems("listAllmeatraw")) {
+            if (stack == null || stack.getItem() == null) continue;
+            String id = Item.itemRegistry.getNameForObject(stack.getItem());
+            if (id.startsWith("harvestcraft:") && id.endsWith("Item")) {
+                IntegrationMapper.addMapping(stack, 64);
+            }
+        }
+
+        for (String od : OreDictionary.getOreNames()) {
+            if (od == null || !od.startsWith("crop")) continue;
+            for (ItemStack stack : ItemHelper.getODItems(od)) {
+                if (stack == null || stack.getItem() == null) continue;
+                String id = Item.itemRegistry.getNameForObject(stack.getItem());
+                if (id.startsWith("harvestcraft:") && id.endsWith("Item")) {
+                    IntegrationMapper.addMapping(stack, 64);
+                }
             }
         }
     }
