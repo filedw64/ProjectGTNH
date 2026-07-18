8028162805f714ece14a714f4ca48a383738a1cd
filedw64 <1356100126@qq.com>
2026-07-18 11:42:01 +0800

    Fix bug

--- src/main/java/moze_intel/projecte/utils/ItemSearchHelperNEI.java
+++ src/main/java/moze_intel/projecte/utils/ItemSearchHelperNEI.java
@@ -14,23 +14,7 @@ public class ItemSearchHelperNEI extends ItemSearchHelper
 	public ItemSearchHelperNEI(String searchString)
 	{
 		super(searchString);
-		filter = getFilter(searchString);
-	}
-
-	public ItemFilter getFilter(String s_filter)
-	{
-		//based on https://github.com/Chicken-Bones/NotEnoughItems/blob/a1879a96548d17f5c4d95b40956d68f6f9db82f8/src/codechicken/nei/SearchField.java#L124-L139
-		List<ItemFilter> primary = new LinkedList<>();
-		List<ItemFilter> secondary = new LinkedList<>();
-		for (SearchField.ISearchProvider p : SearchField.searchProviders) {
-			ItemFilter filter = p.getFilter(s_filter);
-			if (filter != null)
-				(p.isPrimary() ? primary : secondary).add(filter);
-		}
-
-		if (!primary.isEmpty()) return new ItemList.AnyMultiItemFilter(primary);
-		if (!secondary.isEmpty()) return new ItemList.AnyMultiItemFilter(secondary);
-		return new ItemList.EverythingItemFilter();
+		filter = SearchField.getFilter(searchString);
 	}
 
 	@Override
