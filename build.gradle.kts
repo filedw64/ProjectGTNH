73b7e7fe04188c93e06c35e3462d24e1b8fae584
filedw64 <1356100126@qq.com>
2026-07-29 16:04:01 +0800

    update compile args

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,7 @@ tasks.wrapper {
 tasks.reobfJar {
     archiveFileName.set("ProjectE-${project.properties["modVersion"]}.jar")
 }
+tasks.compileJava {
+	options.compilerArgs.add("-Xlint:deprecation")
+	options.compilerArgs.add("-Xlint:unchecked")
+}
