9373ba4cd2fa3f38ca5cc104043953ea4e332503
filedw64 <1356100126@qq.com>
2026-08-06 17:34:39 +0800

    remove unused func

--- src/main/java/moze_intel/projecte/integration/GregTech/GTItemHelper.java
+++ src/main/java/moze_intel/projecte/integration/GregTech/GTItemHelper.java
@@ -1,12 +1,14 @@
 package moze_intel.projecte.integration.GregTech;
 
 import moze_intel.projecte.emc.EMCMapper;
+import moze_intel.projecte.emc.FluidSimpleStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class GTItemHelper {
+
     public static boolean isGTtool(Item item) {
         if (item == null) return false;
 		String id = Item.itemRegistry.getNameForObject(item);
@@ -18,15 +20,18 @@ public class GTItemHelper {
         if (is == null) return false;
         return isGTtool(is.getItem());
     }
-
-    public static boolean isGTtool(int id) {
-        if (id < 0) return false;
-        return isGTtool(Item.getItemById(id));
-    }
-
-    public static boolean isNullGTtool(ItemStack is) {
-        return isGTtool(is) && (!is.hasTagCompound() || is.stackTagCompound.hasNoTags());
-    }
+	
+	public static boolean isGTfluidDisplay(Item item) {
+		if (item == null) return false;
+		String id = Item.itemRegistry.getNameForObject(item);
+		if (id == null) return false;
+		return id.equals("gregtech:gt.GregTech_FluidDisplay");
+	}
+	
+	public static boolean isGTfluidDisplay(ItemStack is) {
+		if (is == null) return false;
+		return isGTfluidDisplay(is.getItem());
+	}
 
     public static double GTtoolEMC(ItemStack is) {
         GTSimpleStack ss = new GTSimpleStack(is);
@@ -50,5 +55,16 @@ public class GTItemHelper {
 				return true;
 		}
 		return false;
+	}
+	
+	public static double GTfluidDisplayEMC(ItemStack is) {
+		if (!isGTfluidDisplay(is)) return 0.0;
+		if (is.stackTagCompound == null || is.stackTagCompound.hasNoTags()) return 0.0;
+		NBTTagCompound nbt = is.getTagCompound();
+		long amount = nbt.getLong("mFluidDisplayAmount");
+		if (amount <= 0) return 0.0;
+		FluidSimpleStack ss = new FluidSimpleStack(is.getItemDamage(), 1);
+		if (!EMCMapper.mapContains(ss)) return 0.0;
+		return EMCMapper.getEmcValue(ss) * amount;
 	}
 }
