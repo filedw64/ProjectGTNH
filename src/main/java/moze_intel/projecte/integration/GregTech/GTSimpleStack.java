0440354855883f8f2cc13235e89e3b2c91bbc852
filedw64 <1356100126@qq.com>
2026-08-06 17:35:14 +0800

    override GTSimpleStack.toItemStack

--- src/main/java/moze_intel/projecte/integration/GregTech/GTSimpleStack.java
+++ src/main/java/moze_intel/projecte/integration/GregTech/GTSimpleStack.java
@@ -6,8 +6,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class GTSimpleStack extends SimpleStack {
-    public String primary;
-    public String secondary;
+    public String primary = "";
+    public String secondary = "";
 
     public GTSimpleStack(int id, int qnty, int damage, String primary, String secondary) {
         super(id, qnty, damage);
@@ -17,15 +17,37 @@ public class GTSimpleStack extends SimpleStack {
 
     public GTSimpleStack(ItemStack is) {
         super(is);
-        if(!GTItemHelper.isGTtool(is) || !is.hasTagCompound()) return;
+        if (is.stackTagCompound == null || is.stackTagCompound.hasNoTags()) {
+			id = -1;
+			return;
+		}
         NBTTagCompound nbt = is.getTagCompound().getCompoundTag("GT.ToolStats");
         primary = nbt.getString("PrimaryMaterial");
         secondary = nbt.getString("SecondaryMaterial");
     }
+	
+	@Override
+	public ItemStack toItemStack() {
+		if (!isValid()) return null;
+
+		Item item = Item.getItemById(id);
+		if (item != null) {
+			ItemStack is = new ItemStack(item, qnty, damage);
+			NBTTagCompound toolStats = new NBTTagCompound();
+			toolStats.setString("PrimaryMaterial", primary);
+			toolStats.setString("SecondaryMaterial", secondary);
+			NBTTagCompound nbt = new NBTTagCompound();
+			nbt.setTag("GT.ToolStats", toolStats);
+			is.stackTagCompound = nbt;
+			return is;
+		}
+
+		return null;
+	}
 
     @Override
     public int hashCode() {
-        return (primary + secondary).hashCode() ^ id;
+        return (primary + secondary).hashCode() ^ damage;
     }
 
     @Override
@@ -48,6 +70,6 @@ public class GTSimpleStack extends SimpleStack {
             return Item.itemRegistry.getNameForObject(item) + " " + qnty + " " + damage + " " + primary + " " + secondary;
         }
 
-        return "id:" + id + " damage:" + damage + " qnty:" + qnty + " primary:" + primary + " secondary" + secondary;
+        return "id:" + id + " damage:" + damage + " qnty:" + qnty + " primary:" + primary + " secondary:" + secondary;
     }
 }
