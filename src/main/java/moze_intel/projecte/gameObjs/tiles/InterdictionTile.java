ea8e0e10d45733e82944cd668ccc6c84bf2fad64
Windy-Bye <WindyBye@outlook.com>
2026-08-12 20:02:25 +0800

    MIsc

--- src/main/java/moze_intel/projecte/gameObjs/tiles/InterdictionTile.java
+++ src/main/java/moze_intel/projecte/gameObjs/tiles/InterdictionTile.java
@@ -8,12 +8,22 @@ public class InterdictionTile extends TileEntity
 {
 	private AxisAlignedBB effectBounds = null;
 
+	private int tickOffset = 0;
+
 	public void updateEntity()
 	{
 		if (effectBounds == null)
 		{
 			effectBounds = AxisAlignedBB.getBoundingBox(xCoord - 8, yCoord - 8, zCoord - 8, xCoord + 8, yCoord + 8, zCoord + 8);
+
+			tickOffset = Math.abs((xCoord * 31 + yCoord * 7 + zCoord) % 2);
 		}
+
+		if ((worldObj.getTotalWorldTime() + tickOffset) % 2 != 0)
+		{
+			return;
+		}
+
 		WorldHelper.repelEntitiesInAABBFromPoint(worldObj, effectBounds, xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, false);
 	}
 }
