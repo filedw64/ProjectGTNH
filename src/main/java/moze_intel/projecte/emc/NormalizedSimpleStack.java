248b9d4f0628fcf3e8dada641ff8dddddda3a62b
Windy-Bye <WindyBye@outlook.com>
2026-08-12 17:44:40 +0800

    Memory&Object-Allocation Optimization

--- src/main/java/moze_intel/projecte/emc/NormalizedSimpleStack.java
+++ src/main/java/moze_intel/projecte/emc/NormalizedSimpleStack.java
@@ -1,11 +1,16 @@
 package moze_intel.projecte.emc;
 
 import moze_intel.projecte.emc.collector.IMappingCollector;
+import moze_intel.projecte.integration.GregTech.GTItemHelper;
+import moze_intel.projecte.integration.GregTech.GTNSSItem;
 import moze_intel.projecte.utils.ItemHelper;
 import moze_intel.projecte.utils.PELogger;
+import moze_intel.projecte.config.ProjectEConfig;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -17,6 +22,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public abstract class NormalizedSimpleStack {
@@ -25,23 +31,27 @@ public abstract class NormalizedSimpleStack {
 		idWithUsedMeta.forEach((id, metaSet) -> {
 			metaSet.remove(OreDictionary.WILDCARD_VALUE);
 			metaSet.add(0);
-			NormalizedSimpleStack stackWildcard = new NSSItem(id, OreDictionary.WILDCARD_VALUE);
+			NormalizedSimpleStack stackWildcard = forItem(id, OreDictionary.WILDCARD_VALUE, null);
 			for (int metadata : metaSet) {
-				mapper.addConversion(1, stackWildcard, Collections.singletonList(new NSSItem(id, metadata)));
+				mapper.addConversion(1, stackWildcard, Collections.singletonList(forItem(id, metadata, null)));
 			}
 		});
 
 		oreDictMap.forEach((odName, nssOre) -> {
 			List<ItemStack> list = ItemHelper.getODItems(odName);
 			for (ItemStack is: list) {
-				mapper.addConversion(1, nssOre, Collections.singletonList(NormalizedSimpleStack.forItem(is)));
-				mapper.addConversion(1, NormalizedSimpleStack.forItem(is), Collections.singletonList(nssOre));
+				NormalizedSimpleStack nssItem = NormalizedSimpleStack.forItem(is);
+				if (nssItem != null) {
+					mapper.addConversion(1, nssOre, Collections.singletonList(nssItem));
+					mapper.addConversion(1, nssItem, Collections.singletonList(nssOre));
+				}
 			}
 		});
 	}
 
 	public static void clearMap() {
 		idWithUsedMeta.clear();
+		itemMap.clear(); // 清理新增的 item 缓存
 		fakeMap.clear();
 		fluidMap.clear();
 		oreDictMap.clear();
@@ -59,23 +69,27 @@ public abstract class NormalizedSimpleStack {
 	public abstract String json();
 
 	private static final Map<String, Set<Integer>> idWithUsedMeta = new HashMap<>();
+	// 引入全局缓存
+	private static final Map<NSSItem, NSSItem> itemMap = new HashMap<>();
 
-	public static NSSItem forItem(String itemName, int damage) {
+	public static NSSItem forItem(String itemName, int damage, NBTTagCompound nbt) {
 		if (Item.itemRegistry.getObject(itemName) == null) {
 			PELogger.logError("Could not create NSSItem: %s", itemName);
 			return null;
 		}
-		NSSItem nss = new NSSItem(itemName, damage);
-		Set<Integer> usedMeta;
-		if (!idWithUsedMeta.containsKey(itemName)) {
-			usedMeta = new HashSet<>();
-			idWithUsedMeta.put(itemName, usedMeta);
-		}
-		else {
-			usedMeta = idWithUsedMeta.get(itemName);
-		}
-		usedMeta.add(damage);
+
+		NSSItem temp = new NSSItem(itemName, damage, nbt);
+		// 复用已存在的相同 NSSItem
+		NSSItem nss = itemMap.computeIfAbsent(temp, k -> k);
+
+		// 使用 computeIfAbsent 替换 containsKey + put
+		idWithUsedMeta.computeIfAbsent(itemName, k -> new HashSet<>()).add(damage);
+
 		return nss;
+	}
+
+	public static NSSItem forItem(String itemName, int damage) {
+		return forItem(itemName, damage, null);
 	}
 
 	public static NSSItem forItem(Block block) {
@@ -100,106 +114,79 @@ public abstract class NormalizedSimpleStack {
 
 	public static NSSItem forItem(ItemStack stack) {
 		if (stack == null || stack.getItem() == null) return null;
-//		if (GTItemHelper.isGTtool(stack))
-//			return new GTNSSItem(stack);
-		NBTTagCompound nbt = ItemHelper.filterNBT(stack);
-		if (nbt != null)
-			return new NBTNSSItem(stack, nbt);
-		return forItem(stack.getItem(), stack.getItemDamage());
+		if (GTItemHelper.isGTtool(stack))
+			return new GTNSSItem(stack); // GTNSSItem 内部逻辑
+
+		String id = Item.itemRegistry.getNameForObject(stack.getItem());
+		return forItem(id, stack.getItemDamage(), ProjectEConfig.getFilteredNBT(stack));
 	}
 
-	public static class NBTNSSItem extends NSSItem {
-		/**
-		 * never try to change key-value in this nbt, or it will cause severe problems!
-		 */
+	public static class NSSItem extends NormalizedSimpleStack {
+		public final String itemName;
+		public final int damage;
 		public final NBTTagCompound nbt;
+		// 预先计算并缓存 HashCode
+		private final int cachedHash;
 
-		protected NBTNSSItem(String itemName, int damage, NBTTagCompound nbt) {
-			super(itemName, damage);
-			this.nbt = (NBTTagCompound) nbt.copy();
-		}
+		protected NSSItem(String itemName, int damage, NBTTagCompound nbt) {
+			this.itemName = itemName;
+			this.damage = damage;
+			this.nbt = nbt;
 
-		private NBTNSSItem(ItemStack stack, NBTTagCompound filtered) {
-			super(Item.itemRegistry.getNameForObject(stack.getItem()), stack.getItemDamage());
-			nbt = filtered;
+			int code = itemName.hashCode() ^ damage;
+			if (nbt != null) {
+				code = code * 31 + nbt.hashCode();
+			}
+			this.cachedHash = code;
 		}
 
 		@Override
 		public int hashCode() {
-			return (itemName.hashCode() ^ damage) * 31 + nbt.hashCode();
+			return cachedHash;
 		}
 
 		@Override
 		public boolean equals(Object obj) {
-			if (obj instanceof NBTNSSItem other) {
-				return this.itemName.equals(other.itemName) && this.damage == other.damage && this.nbt.equals(other.nbt);
+			// 快速引用比对
+			if (this == obj) return true;
+			if (obj instanceof NSSItem other) {
+				return this.damage == other.damage
+					&& this.itemName.equals(other.itemName)
+					&& Objects.equals(this.nbt, other.nbt); // 简化 NBT 比对逻辑
 			}
 			return false;
 		}
 
 		@Override
 		public String json() {
-			return String.format("%s|%s%s", itemName, damage == OreDictionary.WILDCARD_VALUE ? "*" : damage, nbt);
+			String base = String.format("%s|%s", itemName, damage == OreDictionary.WILDCARD_VALUE ? "*" : damage);
+			if (nbt != null) {
+				base += "|" + nbt.toString();
+			}
+			return base;
 		}
 
 		@Override
 		public String toString() {
 			Object obj = Item.itemRegistry.getObject(itemName);
 			return String.format("%s(%s:%s)%s", itemName, Item.itemRegistry.getIDForObject(obj),
-				damage == OreDictionary.WILDCARD_VALUE ? "*" : damage, nbt);
-		}
-	}
-
-	public static class NSSItem extends NormalizedSimpleStack {
-		public final String itemName;
-		public final int damage;
-
-		protected NSSItem(String itemName, int damage) {
-			this.itemName = itemName;
-			this.damage = damage;
-		}
-
-		@Override
-		public int hashCode() {
-			return itemName.hashCode() ^ damage;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof NSSItem other) {
-				return this.itemName.equals(other.itemName) && this.damage == other.damage;
-			}
-			return false;
-		}
-
-		@Override
-		public String json() {
-			return String.format("%s|%s", itemName, damage == OreDictionary.WILDCARD_VALUE ? "*" : damage);
-		}
-
-		@Override
-		public String toString() {
-			Object obj = Item.itemRegistry.getObject(itemName);
-			return String.format("%s(%s:%s)", itemName, Item.itemRegistry.getIDForObject(obj),
-				damage == OreDictionary.WILDCARD_VALUE ? "*" : damage);
+				damage == OreDictionary.WILDCARD_VALUE ? "*" : damage,
+				nbt != null ? " " + nbt.toString() : "");
 		}
 	}
 
 	private static final Map<String, NSSFake> fakeMap = new HashMap<>();
 
 	public static NSSFake forFake(String desc) {
-		if (fakeMap.containsKey(desc))
-			return fakeMap.get(desc);
-		NSSFake nss = new NSSFake(desc);
-		fakeMap.put(desc, nss);
-		return nss;
+		// 使用 computeIfAbsent
+		return fakeMap.computeIfAbsent(desc, NSSFake::new);
 	}
 
 	public static class NSSFake extends NormalizedSimpleStack {
 		private static int fakeItemCounter = 0;
-
 		public final String desc;
 		public final int counter;
+
 		public NSSFake(String desc) {
 			this.desc = desc;
 			this.counter = (++fakeItemCounter);
@@ -230,11 +217,8 @@ public abstract class NormalizedSimpleStack {
 
 	public static NSSFluid forFluid(Fluid fluid) {
 		if (fluid == null) return null;
-		if (fluidMap.containsKey(fluid))
-			return fluidMap.get(fluid);
-		NSSFluid nss = new NSSFluid(fluid);
-		fluidMap.put(fluid, nss);
-		return nss;
+		// 使用 computeIfAbsent
+		return fluidMap.computeIfAbsent(fluid, NSSFluid::new);
 	}
 
 	public static NSSFluid forFluid(FluidStack stack) {
@@ -249,12 +233,16 @@ public abstract class NormalizedSimpleStack {
 
 	public static class NSSFluid extends NormalizedSimpleStack {
 		public final String name;
+		private final int cachedHash;
+
 		private NSSFluid(Fluid f) {
 			this.name = f.getName();
+			this.cachedHash = this.name.hashCode();
 		}
 
 		@Override
 		public boolean equals(Object o) {
+			if (this == o) return true;
 			if (o instanceof NSSFluid other) {
 				return name.equals(other.name);
 			}
@@ -262,14 +250,13 @@ public abstract class NormalizedSimpleStack {
 		}
 
 		@Override
-		public String json()
-		{
+		public String json() {
 			return "FLUID|" + this.name;
 		}
 
 		@Override
 		public int hashCode() {
-			return this.name.hashCode();
+			return cachedHash;
 		}
 
 		@Override
@@ -281,33 +268,31 @@ public abstract class NormalizedSimpleStack {
 	private static final Map<String, NSSOreDictionary> oreDictMap = new HashMap<>();
 
 	public static NSSOreDictionary forOreDictionary(String odName) {
-		if (oreDictMap.containsKey(odName))
-			return oreDictMap.get(odName);
 		List<ItemStack> list = OreDictionary.getOres(odName);
 		if (list == null || list.isEmpty()) {
 			return null;
 		}
-		NSSOreDictionary nss = new NSSOreDictionary(odName);
-		oreDictMap.put(odName, nss);
-		return nss;
+		// 使用 computeIfAbsent
+		return oreDictMap.computeIfAbsent(odName, NSSOreDictionary::new);
 	}
 
 	public static class NSSOreDictionary extends NormalizedSimpleStack {
 		public final String od;
+		private final int cachedHash;
 
 		private NSSOreDictionary(String od) {
 			this.od = od;
+			this.cachedHash = od.hashCode();
 		}
 
 		@Override
-		public int hashCode()
-		{
-			return od.hashCode();
+		public int hashCode() {
+			return cachedHash;
 		}
 
 		@Override
-		public boolean equals(Object o)
-		{
+		public boolean equals(Object o) {
+			if (this == o) return true;
 			if (o instanceof NSSOreDictionary other) {
 				return this.od.equals(other.od);
 			}
@@ -315,8 +300,7 @@ public abstract class NormalizedSimpleStack {
 		}
 
 		@Override
-		public String json()
-		{
+		public String json() {
 			return "OD|" + this.od;
 		}
 
@@ -327,9 +311,9 @@ public abstract class NormalizedSimpleStack {
 	}
 
 	public static NSSItem fromJson(String jsonStr) {
-		int pipeIndex = jsonStr.indexOf('|');
-		String name = jsonStr.substring(0, pipeIndex);
-		String metaStr = jsonStr.substring(pipeIndex + 1).split("\\{")[0];
+		String[] parts = jsonStr.split("\\|", 3);
+		String name = parts[0];
+		String metaStr = parts[1].split("\\{")[0];
 		int itemDamage;
 		if (metaStr.equals("*")) {
 			itemDamage = OreDictionary.WILDCARD_VALUE;
@@ -342,6 +326,16 @@ public abstract class NormalizedSimpleStack {
 				return null;
 			}
 		}
-		return forItem(name, itemDamage);
+
+		NBTTagCompound nbt = null;
+		if (parts.length == 3) {
+			try {
+				nbt = (NBTTagCompound) JsonToNBT.func_150315_a(parts[2]);
+			} catch (NBTException e) {
+				PELogger.logError("Cannot parse NBT for NSSItem %s: %s", jsonStr, e);
+			}
+		}
+
+		return forItem(name, itemDamage, nbt);
 	}
 }
--- src/main/java/moze_intel/projecte/emc/arithmetics/DoubleArithmetic.java
+++ src/main/java/moze_intel/projecte/emc/arithmetics/DoubleArithmetic.java
@@ -1,40 +1,58 @@
 package moze_intel.projecte.emc.arithmetics;
 
-public class DoubleArithmetic implements IValueArithmetic<Double>{
-    @Override
-    public boolean isZero(Double value) {
-        return value == 0.0;
-    }
+public class DoubleArithmetic implements IValueArithmetic<Double> {
 
-    @Override
-    public Double getZero() {
-        return 0.0;
-    }
+	// 提供单例实例
+	public static final DoubleArithmetic INSTANCE = new DoubleArithmetic();
 
-    @Override
-    public Double add(Double a, Double b) {
-        return a + b;
-    }
+	// 缓存常用的 Double 对象
+	private static final Double ZERO = 0.0;
+	private static final Double FREE = -Double.MAX_VALUE;
 
-    @Override
-    public Double mul(int a, Double b) {
-        if (isFree(b)) return getFree();
-        return a * b;
-    }
+	@Override
+	public boolean isZero(Double value) {
+		return value != null && value.doubleValue() == 0.0;
+	}
 
-    @Override
-    public Double div(Double a, int b) {
-        if (isFree(a)) return getFree();
-        return a / b;
-    }
+	@Override
+	public Double getZero() {
+		// 返回缓存的常量
+		return ZERO;
+	}
 
-    @Override
-    public Double getFree() {
-        return -Double.MAX_VALUE;
-    }
+	@Override
+	public Double add(Double a, Double b) {
+		if (isFree(a)) return b;
+		if (isFree(b)) return a;
+		if (isZero(a)) return b;
+		if (isZero(b)) return a;
+		return a + b;
+	}
 
-    @Override
-    public boolean isFree(Double value) {
-        return value == -Double.MAX_VALUE;
-    }
+	@Override
+	public Double mul(int a, Double b) {
+		if (a == 0 || isZero(b)) return ZERO;
+		if (isFree(b)) return FREE;
+		return a * b;
+	}
+
+	@Override
+	public Double div(Double a, int b) {
+		if (isZero(a)) return ZERO;
+		if (isFree(a)) return FREE;
+		// 防止除以 0 导致返回 Infinity
+		if (b == 0) return ZERO;
+		return a / b;
+	}
+
+	@Override
+	public Double getFree() {
+		// 返回缓存的常量
+		return FREE;
+	}
+
+	@Override
+	public boolean isFree(Double value) {
+		return value != null && value.doubleValue() == -Double.MAX_VALUE;
+	}
 }
