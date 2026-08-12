9d72504815da2cb351a47f3948e588df45b2a3e7
Windy-Bye <WindyBye@outlook.com>
2026-08-12 15:01:25 +0800

    QoL features

--- src/main/java/moze_intel/projecte/events/PlayerEvents.java
+++ src/main/java/moze_intel/projecte/events/PlayerEvents.java
@@ -5,6 +5,7 @@ import moze_intel.projecte.PECore;
 import moze_intel.projecte.gameObjs.ObjHandler;
 import moze_intel.projecte.gameObjs.container.AlchBagContainer;
 import moze_intel.projecte.gameObjs.items.AlchemicalBag;
+import moze_intel.projecte.gameObjs.items.tools.PEToolBase;
 import moze_intel.projecte.handlers.PlayerChecks;
 import moze_intel.projecte.playerData.AlchBagProps;
 import moze_intel.projecte.playerData.AlchemicalBags;
@@ -29,6 +30,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
@@ -54,7 +56,7 @@ public class PlayerEvents {
 	public void onEntityJoinWorld(EntityJoinWorldEvent event) {
 		Entity ent = event.entity;
 		if (!ent.worldObj.isRemote && ent instanceof EntityPlayerMP player) {
-            Transmutation.sync(player);
+			Transmutation.sync(player);
 			AlchemicalBags.syncFull(player);
 		}
 	}
@@ -138,6 +140,32 @@ public class PlayerEvents {
 				AlchemicalBags.syncPartial(player, bag.getItemDamage());
 
 				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onItemToss(ItemTossEvent event) {
+		EntityPlayer player = event.player;
+		ItemStack stack = event.entityItem.getEntityItem();
+
+		// 判断丢出的物品是否为我们需要保护的工具/武器
+		if (stack != null && stack.getItem() instanceof PEToolBase) {
+
+			// 如果没有打开额外的GUI（如箱子、合成台），openContainer 就是玩家自身的 inventoryContainer
+			if (player.openContainer == player.inventoryContainer) {
+
+				// 取消抛出事件，这会阻止掉落物实体被加入世界
+				event.setCanceled(true);
+				event.entityItem.setDead();
+
+				// 将物品重新塞回玩家背包
+				player.inventory.addItemStackToInventory(stack);
+
+				// 在服务端强制同步玩家背包，利用 Java 16+ 的 instanceof 模式匹配消除强转警告
+				if (!player.worldObj.isRemote && player instanceof EntityPlayerMP playerMP) {
+					playerMP.inventoryContainer.detectAndSendChanges();
+				}
 			}
 		}
 	}
