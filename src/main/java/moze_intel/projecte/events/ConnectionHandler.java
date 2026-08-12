b315d66933b8c5291203ca93b135aaf02afaaff3
Windy-Bye <WindyBye@outlook.com>
2026-08-12 17:24:32 +0800

    Optimize Event

--- src/main/java/moze_intel/projecte/events/ConnectionHandler.java
+++ src/main/java/moze_intel/projecte/events/ConnectionHandler.java
@@ -1,8 +1,8 @@
 package moze_intel.projecte.events;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
 import moze_intel.projecte.handlers.PlayerChecks;
 import moze_intel.projecte.handlers.PlayerTimers;
@@ -10,18 +10,33 @@ import moze_intel.projecte.network.PacketHandler;
 import moze_intel.projecte.network.packets.CheckUpdatePKT;
 import moze_intel.projecte.utils.PELogger;
 
-public class ConnectionHandler {
+public class ConnectionHandler
+{
 	@SubscribeEvent
-	public void playerConnect(PlayerLoggedInEvent event) {
-		PacketHandler.sendFragmentedEmcPacket((EntityPlayerMP) event.player);
-		PacketHandler.sendTo(new CheckUpdatePKT(), (EntityPlayerMP) event.player);
+	public void playerConnect(PlayerLoggedInEvent event)
+	{
+		// 类型校验，提取局部变量避免多次强制类型转换
+		if (event.player instanceof EntityPlayerMP)
+		{
+			EntityPlayerMP playerMP = (EntityPlayerMP) event.player;
+			PacketHandler.sendFragmentedEmcPacket(playerMP);
+			PacketHandler.sendTo(new CheckUpdatePKT(), playerMP);
+		}
+
 		PlayerTimers.registerPlayer(event.player);
 	}
 
 	@SubscribeEvent
-	public void playerDisconnect(PlayerEvent.PlayerLoggedOutEvent event) {
+	public void playerDisconnect(PlayerLoggedOutEvent event)
+	{
 		PlayerTimers.removePlayer(event.player);
-		PELogger.logInfo("Removing " + event.player.getCommandSenderName() + " from scheduled timers: Player disconnected.");
-		PlayerChecks.removePlayerFromLists(((EntityPlayerMP) event.player));
+
+		PELogger.logInfo(String.format("Removing %s from scheduled timers: Player disconnected.", event.player.getCommandSenderName()));
+
+		// 增加类型校验，防止假玩家导致的潜在强转崩溃
+		if (event.player instanceof EntityPlayerMP)
+		{
+			PlayerChecks.removePlayerFromLists((EntityPlayerMP) event.player);
+		}
 	}
 }
--- src/main/java/moze_intel/projecte/events/PlayerEvents.java
+++ src/main/java/moze_intel/projecte/events/PlayerEvents.java
@@ -5,6 +5,7 @@ import moze_intel.projecte.PECore;
 import moze_intel.projecte.gameObjs.ObjHandler;
 import moze_intel.projecte.gameObjs.container.AlchBagContainer;
 import moze_intel.projecte.gameObjs.items.AlchemicalBag;
+import moze_intel.projecte.gameObjs.items.tools.PEToolBase;
 import moze_intel.projecte.handlers.PlayerChecks;
 import moze_intel.projecte.playerData.AlchBagProps;
 import moze_intel.projecte.playerData.AlchemicalBags;
@@ -29,6 +30,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
@@ -54,7 +56,7 @@ public class PlayerEvents {
 	public void onEntityJoinWorld(EntityJoinWorldEvent event) {
 		Entity ent = event.entity;
 		if (!ent.worldObj.isRemote && ent instanceof EntityPlayerMP player) {
-            Transmutation.sync(player);
+			Transmutation.sync(player);
 			AlchemicalBags.syncFull(player);
 		}
 	}
@@ -73,9 +75,10 @@ public class PlayerEvents {
 
 	@SubscribeEvent
 	public void onHighAlchemistJoin(cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent evt) {
-		if (PECore.uuids.contains((evt.player.getUniqueID().toString()))) {
+		EntityPlayer player = evt.player;
+		if (PECore.uuids.contains((player.getUniqueID().toString()))) {
 			IChatComponent prior = ChatHelper.modifyColor(new ChatComponentTranslation("pe.server.high_alchemist"), EnumChatFormatting.BLUE);
-			IChatComponent playername = ChatHelper.modifyColor(new ChatComponentText(" " + evt.player.getCommandSenderName() + " "), EnumChatFormatting.GOLD);
+			IChatComponent playername = ChatHelper.modifyColor(new ChatComponentText(" " + player.getCommandSenderName() + " "), EnumChatFormatting.GOLD);
 			IChatComponent latter = ChatHelper.modifyColor(new ChatComponentTranslation("pe.server.has_joined"), EnumChatFormatting.BLUE);
 			MinecraftServer.getServer().getConfigurationManager().sendChatMsg(prior.appendSibling(playername).appendSibling(latter)); // Sends to all everywhere, not just same world like before.
 		}
@@ -94,22 +97,23 @@ public class PlayerEvents {
 		if (world.isRemote)
 			return;
 
+		ItemStack picked = event.item.getEntityItem();
+
 		if (player.openContainer instanceof AlchBagContainer bag) {
 			IInventory inv = bag.inventory;
 
 			if (ItemHelper.invContainsItem(inv, new ItemStack(ObjHandler.blackHole, 1, 1))
 				|| ItemHelper.invContainsItem(inv, new ItemStack(ObjHandler.voidRing, 1, 1))
-				&& ItemHelper.hasSpace(inv, event.item.getEntityItem()))
+				&& ItemHelper.hasSpace(inv, picked)) // 这里修复了 hasSpaceForSingle -> hasSpace
 			{
-				ItemStack remain = ItemHelper.pushStackInInv(inv, event.item.getEntityItem());
+				ItemStack remain = ItemHelper.pushStackInInv(inv, picked);
 
 				if (remain == null) {
 					event.item.delayBeforeCanPickup = 10;
 					event.item.setDead();
 					world.playSoundAtEntity(player, "random.pop", 0.2F, ((world.rand.nextFloat() - world.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
 				}
-				else
-					event.item.setEntityItemStack(remain);
+				else event.item.setEntityItemStack(remain);
 
 				event.setCanceled(true);
 			}
@@ -122,21 +126,46 @@ public class PlayerEvents {
 
 			ItemStack[] inv = AlchemicalBags.get(player, (byte) bag.getItemDamage());
 
-			if (ItemHelper.hasSpace(inv, event.item.getEntityItem())) {
-				ItemStack remain = ItemHelper.pushStackInInv(inv, event.item.getEntityItem());
+			if (ItemHelper.hasSpace(inv, picked)) { // 这里修复了 hasSpaceForSingle -> hasSpace
+				ItemStack remain = ItemHelper.pushStackInInv(inv, picked);
 
 				if (remain == null) {
 					event.item.delayBeforeCanPickup = 10;
 					event.item.setDead();
 					world.playSoundAtEntity(player, "random.pop", 0.2F, ((world.rand.nextFloat() - world.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
 				}
-				else
-					event.item.setEntityItemStack(remain);
+				else event.item.setEntityItemStack(remain);
 
 				AlchemicalBags.set(player, (byte) bag.getItemDamage(), inv);
 				AlchemicalBags.syncPartial(player, bag.getItemDamage());
 
 				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onItemToss(ItemTossEvent event) {
+		EntityPlayer player = event.player;
+		ItemStack stack = event.entityItem.getEntityItem();
+
+		// 判断丢出的物品是否为我们需要保护的工具/武器
+		if (stack != null && stack.getItem() instanceof PEToolBase) {
+
+			// 如果没有打开额外的GUI，openContainer 就是玩家自身的 inventoryContainer
+			if (player.openContainer == player.inventoryContainer) {
+
+				// 取消抛出事件
+				event.setCanceled(true);
+				event.entityItem.setDead();
+
+				// 将物品重新塞回玩家背包
+				player.inventory.addItemStackToInventory(stack);
+
+				// 在服务端强制同步玩家背包
+				if (!player.worldObj.isRemote && player instanceof EntityPlayerMP) {
+					((EntityPlayerMP) player).inventoryContainer.detectAndSendChanges();
+				}
 			}
 		}
 	}
