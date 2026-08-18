dce925f1533598ad7f2b8c2e0d828f450e756c3a
filedw64 <1356100126@qq.com>
2026-08-18 20:03:09 +0800

    delete GTNSSItem and GTSimpleStack

--- src/main/java/moze_intel/projecte/utils/EnchantmentBlacklist.java
+++ src/main/java/moze_intel/projecte/utils/EnchantmentBlacklist.java
@@ -1,8 +1,6 @@
 package moze_intel.projecte.utils;
 
 import moze_intel.projecte.emc.SimpleStack;
-import moze_intel.projecte.integration.helpers.GTItemHelper;
-import moze_intel.projecte.integration.GregTech.GTSimpleStack;
 import net.minecraft.item.ItemStack;
 
 import java.util.HashSet;
@@ -12,20 +10,14 @@ public final class EnchantmentBlacklist {
     private static final Set<SimpleStack> blacklist = new HashSet<>();
 
     public static boolean add(ItemStack stack) {
-        SimpleStack ss;
-        if (GTItemHelper.isGTtool(stack))
-            ss = new GTSimpleStack(stack);
-        else ss = new SimpleStack(stack);
+        SimpleStack ss = SimpleStack.getFor(stack);
         if (!ss.isValid()) return false;
         ss.qnty = 1;
         return blacklist.add(ss);
     }
 
     public static boolean contains(ItemStack stack) {
-        SimpleStack ss;
-        if (GTItemHelper.isGTtool(stack))
-            ss = new GTSimpleStack(stack);
-        else ss = new SimpleStack(stack);
+		SimpleStack ss = SimpleStack.getFor(stack);
         if (!ss.isValid()) return false;
         ss.qnty = 1;
         return blacklist.contains(ss);
